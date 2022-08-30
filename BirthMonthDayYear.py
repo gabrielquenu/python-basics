dabd550fb26a2f6379849099ec9b28d082da384a
Gabriel Queiroz Nunes <gabrielqn@pop-os.localdomain>
2022-08-30 20:18:57 -0300

    Simple sum script developed

---
+++ BirthMonthDayYear.py
@@ -0,0 +1,5 @@
+month = input('What month did you birth? ')
+day = input('Whats day did you birth? ')
+year = input('Whats year did you birth? ')
+
+print('Your birth day is', month, '/', day, '/', year)
---
+++ SimpleSum.py
@@ -0,0 +1,5 @@
+A = input('Type the first number: ')
+B = input('Type the second number: ')
+C = int(A) + int(B)
+
+print(A, '+', B, '=', C)
